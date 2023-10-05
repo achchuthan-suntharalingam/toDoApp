ecdb27466eef89aee0f3ebe71210d3a8c034cbc1
achchuthan suntharalingam <achchu0103@gmail.com>
2023-10-05 14:34:42 +0530

    header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,6 +16,19 @@ function NavBar() {
                 <img className="dropdown" src="/icons/Chevron-down.png" alt="dropdown"></img>
             </div>
         </div>
+        <div className="frame1743">
+            <h3 id="welcome">Welcome Back, Jhon Doe</h3>
+            <p id="welcomenote">The end of the year is coming. Are you planning your performance interviews? You can do this super efficiently with Acmy.<br/>
+                <a id="link" href="http://localhost:3000/">Look here for more information</a></p>
+            <div className="group1770">
+                <img id="ellipse1" src="/icons/ellipse1.png" alt=""></img>
+                <img id="ellipse2" src="/icons/ellipse2.png" alt=""></img>
+                <img id="ellipse3" src="/icons/ellipse3.png" alt=""></img>
+                <img id="ellipse4" src="/icons/ellipse4.png" alt=""></img>
+                <img id="ellipse5" src="/icons/ellipse5.png" alt=""></img>
+            </div>
+            <div className="close"><img id="closeicon" src="/icons/close.png" alt="close"></img></div>
+        </div>
     </div>
   )
 }
